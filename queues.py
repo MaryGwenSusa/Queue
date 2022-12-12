9068acecd8eabaf2ffc2817f47d0089c78fba221
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-12 17:32:15 +0800

    Defined a method that adds element onto the empty list with regards to the element's priority value

--- queues.py
+++ queues.py
@@ -1,9 +1,16 @@
 from collections import deque
 from heapq import heappush, heappop
+#heap compares elements by value not by priority so Python’s tuple can be used for comparison which takes into account the tuple’s components
+
 
 class PriorityQueue:
     def __init__(self): #__init__ is automatically used when creating a class
         self._elements = [] #self parameter used to access variables of the class; the underscore on the elements means internal bit of implementation which means it cannot be accessed outside the class/modify
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value)) #somehow works like .append()
+        #the first element on a heap always has the smallest (min-heap) or the highest (max-heap) value, depending on how you define the condition for the mentioned relationship
+        #lower Unicode means the element is smaller
 
 
 
@@ -13,9 +20,8 @@ class PriorityQueue:
 
 
 #heap compares elements by value not by priority so Python’s tuple can be used for comparison which takes into account the tuple’s components
-#the first element on a heap always has the smallest (min-heap) or the highest (max-heap) value, depending on how you define the condition for the mentioned relationship
 #fruits = []
-#heappush(fruits, "orange") #somehow works like .append()
+#heappush(fruits, "orange") 
 #heappush(fruits, "apple")
 #heappush(fruits, "banana")
 
