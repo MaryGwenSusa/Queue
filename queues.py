194dea4db57325be9890461d6ce699e86a80ffbd
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-12 16:05:14 +0800

    Used inheritance to override the defined function dequeue inside class or simply that method to get the last element first: simply, LIFO queue.

--- queues.py
+++ queues.py
@@ -1,10 +1,11 @@
 from collections import deque
 
-#this program is a simple structure of a FIFO queue wherein you add another element to the last index then gets the first element from the left side
-#or the one with zero index
 class Queue: #object constructor
+    """this program is a simple structure of a FIFO queue wherein you add another element to the last index then gets the first element from the left side"""
+    #or the one with zero index
     #class is very useful to somehow make a local variable accessible to every defined functions
     def __init__(self, *elements): #__init__ is automatically used when creating a class; the asterisk beside the elements parameter allows you to provide varying number of positional arguments
+        """here inputting initial elements is allowed"""
         self._elements = deque(elements) #self parameter used to access variables of the class
     
     def __len__(self): # this reports the stack's number of elements
@@ -18,20 +19,27 @@ class Queue: #object constructor
 
     def enqueue(self, element):
         self._elements.append(element) #the underscore on the elements means internal bit of implementation which means it cannot be accessed outside the class/modify
-    
+
+class Stack(Queue):
+    """this program is a simple structure of a LIFO queue hwerein you add naother element to the last index then gets the last element you add first"""
     def dequeue(self):
-        return self._elements.popleft()#gets the first element from the left since this is portrayed in a horizontal manner but still preserves the general idea of a stack
+        #return self._elements.popleft()#gets the first element from the left since this is portrayed in a horizontal manner but still preserves the general idea of a stack
+        return self._elements.pop() #gets the last element
 
-fifo = Queue("1st", "2nd", "3rd") #added arguments to the class itself then transfers to the defined functions' parameter inside
+"""input initial elements"""
+lifo = Stack("1st", "2nd", "3rd") #added arguments to the class itself then transfers to the defined functions' parameter inside
 
-#syntax to calling functions and adding value/argument to the parameter
+lifo.enqueue("4th") #syntax to calling functions and adding value/argument to the parameter
+lifo.enqueue("5th")
+lifo.enqueue("6th")
 
-print(len(fifo))
-print(fifo.__len__()) #another syntax to call the __len__ function inside the class and print its processed data
 
-for element in fifo: #print in loop each element of the stack
+print(len(lifo))
+print(lifo.__len__()) #another syntax to call the __len__ function inside the class and print its processed data
+
+for element in lifo: #print in loop each element of the stack
     print(element)
 
 
-print(len(fifo)) #since there's a function that automatically dequeues the elements, here is a proof of that
+print(len(lifo)) #since there's a function that automatically dequeues the elements, here is a proof of that
 
