4967c7749c85df42a558bf332a7dbf84324d2f45
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-12 17:20:50 +0800

    Created a new class for Priority Queue and defined a method where it initializes an empty list

--- queues.py
+++ queues.py
@@ -1,28 +1,28 @@
+from collections import deque
 from heapq import heappush, heappop
+
+class PriorityQueue:
+    def __init__(self): #__init__ is automatically used when creating a class
+        self._elements = [] #self parameter used to access variables of the class; the underscore on the elements means internal bit of implementation which means it cannot be accessed outside the class/modify
+
+
+
+
+
+
+
+
 #heap compares elements by value not by priority so Python’s tuple can be used for comparison which takes into account the tuple’s components
 #the first element on a heap always has the smallest (min-heap) or the highest (max-heap) value, depending on how you define the condition for the mentioned relationship
-fruits = []
-heappush(fruits, "orange") #somehow works like .append()
-heappush(fruits, "apple")
-heappush(fruits, "banana")
+#fruits = []
+#heappush(fruits, "orange") #somehow works like .append()
+#heappush(fruits, "apple")
+#heappush(fruits, "banana")
 
-print(fruits)
+#print(fruits)
 #lower Unicode means the element is smaller
 #When you pop an element from a heap, you’ll always get the first one, while the remaining elements might shuffle a little bit
-print(heappop(fruits))
+#print(heappop(fruits))
 
 #Notice how the banana and orange swapped places to ensure the first element continues to be the smallest
-print(fruits)
-
-person1 = ("John", "Brown", 42)
-person2 = ("John", "Doe", 42)
-person3 = ("John", "Doe", 24)
-
-if person1 < person2:
-    print(True)
-else:
-    print(False)
-if person2 < person3:
-    print(True)
-else:
-    print(False)
+#print(fruits)
