aee2522653047b3430ad1266a66f573b6fdd8d16
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-12 11:06:15 +0800

    Another defined function for enqueueing that uses the .append function

--- queues.py
+++ queues.py
@@ -1,5 +1,10 @@
 from collections import deque
 
 class Queue: #object constructor
+    """So class is very useful to somehow make a local variable accessible to every defined functions"""
     def __init__(self): #__init__ is automatically used when creating a class
-        self._elements = deque()
+        self._elements = deque() #self parameter used to access variables of the class
+    
+    def enqueue(self, element):
+        self._elements.append(element)
+    
