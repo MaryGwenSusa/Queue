bd059fca03b89bac47e575ea0e58a4138a972151
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-13 23:22:40 +0800

    Inserted the previous traversal FIFO and modified the network to depth-first search. Visibly, there is a difference in sequence

---
+++ depth-fS-LIFO.py
@@ -0,0 +1,46 @@
+import networkx as nx
+from typing import NamedTuple
+
+
+class City(NamedTuple): #extend a named tuple to ensure that your node objects are hashable, which is required by networkx; could also use a properly configured data class
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
+        "this will take a dictionary of attributes extracted from a DOT file and returns a new instance of the City class"
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
+
+def load_graph(filename, node_factory): #callable factory for the node objects like from the City.from_dict() class method
+    graph = nx.nx_agraph.read_dot(filename) #Reads a DOT file
+    #build a mapping of node identifiers to the object-oriented representation of the graph nodes 
+    nodes = {
+        name: node_factory(attributes)
+        for name, attributes in graph.nodes(data=True)
+    }
+    #returns the mapping and a new graph comprising nodes and weighted edges
+    return nodes, nx.Graph(
+        (nodes[name1], nodes[name2], weights)
+        for name1, name2, weights in graph.edges(data=True)
+    )
+
+def is_twentieth_century(year):
+    return year and 1901 <= year <= 2000
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+for node in nx.dfs_tree(graph, nodes["edinburgh"]):
+    print("📍", node.name)
+    if is_twentieth_century(node.year):
+        print("Found:", node.name, node.year)
+        break
+else:
+    print("Not found")
