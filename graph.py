10be5c248be6a7b79cd28922afaf4c7ce90d0a83
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-13 11:10:45 +0800

    Created a method that will take a dictionary of attributes and returns a new instance of the City class

--- graph.py
+++ graph.py
@@ -6,3 +6,14 @@ class City(NamedTuple): #extend a named tuple to ensure that your node objects a
     year: int | None
     latitude: float
     longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
+        "this will take a dictionary of attributes extracted from a DOT file and returns a new instance of the City class"
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
