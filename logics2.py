13d3922c514bed35bd9cf9f8301158af78dbe56a
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-13 21:01:39 +0800

    Notice I made a mistake copying the function; called the loadgraph with intended arguments and stored the values inside variables

--- logics2.py
+++ logics2.py
@@ -1,4 +1,7 @@
 import networkx as nx
+from typing import NamedTuple
+
+
 
 class City(NamedTuple): #extend a named tuple to ensure that your node objects are hashable, which is required by networkx; could also use a properly configured data class
     name: str
@@ -31,3 +34,17 @@ def load_graph(filename, node_factory): #callable factory for the node objects l
         for name1, name2, weights in graph.edges(data=True)
     )
 
+def load_graph(filename, node_factory): #callable factory for the node objects like from the City.from_dict() class method
+    graph = nx.nx_agraph.read_dot(filename) #Reads a DOT file
+    #build a mapping of node identifiers to the object-oriented representation of the graph nodes 
+    nodes = {
+        name: node_factory(attributes)
+        for name, attributes in graph.nodes(data=True)
+    }
+    #returns the mapping and a new graph comprising nodes and weighted edges
+    return nodes, nx.Graph(
+        (nodes[name1], nodes[name2], weights)
+        for name1, name2, weights in graph.edges(data=True)
+    )
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict) #called the function with value arguments then stored in variables
