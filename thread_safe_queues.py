ee76d4ac306330b877aa6f4e8bd750988ea1a509
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-14 19:35:34 +0800

    Defined main function which will receive arguments with the help of argprase module

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -1,8 +1,13 @@
 import argparse
 from queue import LifoQueue, PriorityQueue, Queue
 
+"""this dictionary maps queue names to their respective classes, which you call to create a new queue instance based on the value of a command-line argument"""
 QUEUE_TYPES = {
     "fifo": Queue,
     "lifo": LifoQueue,
     "heap": PriorityQueue
 }
+
+def main(args):
+    """this function is the entry point which receives the parsed arguments supplied by parse_args()"""
+    buffer = QUEUE_TYPES[args.queue]()
