3de7ec80974fe57235cb60a94185bf997765636d
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-14 19:46:56 +0800

    Defined parse_args that will supply parsed arguments to defined main funciton; also called and store to a variable the argparse.ArgumentParser for container to argument specifications which has options that apply the parser as whole

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse #makes it easy to write user-friendly command-line interfaces
 from queue import LifoQueue, PriorityQueue, Queue
 
 """this dictionary maps queue names to their respective classes, which you call to create a new queue instance based on the value of a command-line argument"""
@@ -9,5 +9,10 @@ QUEUE_TYPES = {
 }
 
 def main(args):
-    """this function is the entry point which receives the parsed arguments supplied by parse_args()"""
+    """this function is the entry point which receives the parsed arguments (thru argparse module) supplied by parse_args()"""
     buffer = QUEUE_TYPES[args.queue]()
+
+def parse_args():
+    """this function supplies parsed arguments"""
+    parser = argparse.ArgumentParser() #argparse.ArgumentParser() is a container for argument specifications and has options that apply the parser as whole
+    
