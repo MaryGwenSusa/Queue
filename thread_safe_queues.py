48f59d2b9857b103f6fee6e98bdb6dfcb7bc19d8
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-14 19:57:08 +0800

    Added argument on the parser variable container with specifications and options as positional args under defined parse_args function

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -15,4 +15,9 @@ def main(args):
 def parse_args():
     """this function supplies parsed arguments"""
     parser = argparse.ArgumentParser() #argparse.ArgumentParser() is a container for argument specifications and has options that apply the parser as whole
+    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo") #.add_argument method attaches individual argument specifications to the parser. It supports positional arguments, options that accept values, and on/off flags
+    parser.add_argument("-p", "--producers", type=int, default=3)
+    parser.add_argument("-c", "--consumers", type=int, default=2)
+    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
+    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
     
