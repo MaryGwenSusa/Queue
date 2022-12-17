5b10461a276e55df1597a3ed77f8a4ebc9b10acf
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-17 21:29:05 +0800

    Used try function to test the counter function (as links) as argument for display function (to be defined later) for errors and make sure to close the .Clientsession interface by finally by the help of await and finally function

--- async_queues.py
+++ async_queues.py
@@ -7,3 +7,11 @@ import aiohttp
 async def main(args): #the syntax async def introduces either a native coroutine or an asynchronous generator. the expressions async with and async for are also valid
     session = aiohttp.ClientSession() #client session is the recommended interface for making HTTP requests. session encapsulates a connection pool (connector 
     #instance) and supports keepalives by default
+    try: #the try block lets you test a block of code for errors
+        links = Counter()
+        display(links)
+    finally: #finally block always gets executed either exception is generated or not
+        await session.close() 
+        #await passes function control back to the event loop (it suspends the execution of the surrounding coroutine)
+        #Tclose() method closes an open file. files should always be closed, in some cases, due to buffering, changes made to a file may not show until you
+        #close the file
