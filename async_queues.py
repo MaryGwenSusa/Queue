07a7e001304b38b69c897ebf5e3215ff4d5d7ffa
Mary Gwen Susa <Mary Gwen G. Susa>
2022-12-17 21:52:33 +0800

    Defined a function that would supply parsed arguments; declared a variable for .ArgumentParser() (from argparse module which is a container for argument specifications and has options that apply the parser as a whole--with the help of .add_argument function. Then, be returned according to the argumentsbefore

--- async_queues.py
+++ async_queues.py
@@ -15,3 +15,10 @@ async def main(args): #the syntax async def introduces either a native coroutine
         #await passes function control back to the event loop (it suspends the execution of the surrounding coroutine)
         #Tclose() method closes an open file. files should always be closed, in some cases, due to buffering, changes made to a file may not show until you
         #close the file
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+    parser.add_argument("-d", "--max-depth", type=int, default=2)
+    parser.add_argument("-w", "--num-workers", type=int, default=3)
+    return parser.parse_args()
